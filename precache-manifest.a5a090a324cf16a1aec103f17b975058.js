c85c117879e84e9ae00da00cdeca3838e0a646d5
Md. Alamgir Hossain <alamgirakash2000@gmail.com>
2020-09-16 00:23:44 +0600

    Updates

---
+++ precache-manifest.a5a090a324cf16a1aec103f17b975058.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a751cacc34337634926c5bd4fa93398",
+    "url": "/12-travel-guru/index.html"
+  },
+  {
+    "revision": "d77b4954c6efe83cc126",
+    "url": "/12-travel-guru/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9d9af57014d3fca92250",
+    "url": "/12-travel-guru/static/js/2.021c4625.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/12-travel-guru/static/js/2.021c4625.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d77b4954c6efe83cc126",
+    "url": "/12-travel-guru/static/js/main.53715126.chunk.js"
+  },
+  {
+    "revision": "938708c6860cd99767d3",
+    "url": "/12-travel-guru/static/js/runtime-main.391d0783.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/12-travel-guru/static/media/logo.5d5d9eef.svg"
+  }
+]);
